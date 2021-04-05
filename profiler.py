6eb9a31be29680114274a241042192d7397b667f
GROV Håkon <hakon.grov@soprasteria.com>
2021-04-05 20:35:47 +0200

    Adds a profiler to optimize code and find weak points

---
+++ profiler.py
@@ -0,0 +1,15 @@
+from game import Game
+import cProfile, pstats,io
+
+if __name__ == "__main__":
+    pr = cProfile.Profile()
+    pr.enable()
+
+    Game(size = 4, play_type = 2, start_player = 1, plotter = False, simulations = 10000)
+
+    pr.disable()
+    s = io.StringIO()
+    sortby = pstats.SortKey.TIME
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats(15)
+    print(s.getvalue())

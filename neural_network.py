dc12f97569c1b1223cc18bfb21c545bc59db2c41
GROV Håkon <hakon.grov@soprasteria.com>
2021-05-06 22:12:10 +0200

    Splits out model creation and creates and train model based on board size

--- neural_network.py
+++ neural_network.py
@@ -9,22 +9,32 @@ class hex_neural_network:
     def __init__(self, grid_size):
         self.train_x = None
         self.train_y = None
-        self.grid_size = grid_size + 1
-        self.model = keras.Sequential(
-            [
-                layers.Dense(9, activation="tanh", input_shape=(self.grid_size,), name="layer1"),
-                layers.Dense(6, activation="tanh", name="layer2"),
-                layers.Dense(grid_size, name="layer3"),
-            ]
-        )
-        self.model.compile(
-            optimizer='adam',
-            loss='mean_squared_error',
-            metrics=["accuracy"]
-        )
+        self.model = None
+        self.model_size = grid_size
+        self. grid_size = grid_size * grid_size
+
+
+    def create_model(self):
+        try:
+            self.load_model()
+            print("Model loaded")
+        except:
+            print("No model created. Creating new model")
+            self.model = keras.Sequential(
+                [
+                    layers.Dense(6, activation="tanh", input_shape=(self.grid_size,), name="layer1"),
+                    layers.Dense(3, activation="tanh", name="layer2"),
+                    layers.Dense(self.grid_size, name="layer3"),
+                ]
+            )
+            self.model.compile(
+                optimizer='adam',
+                loss='mean_squared_error',
+                metrics=["accuracy"]
+            )
 
     def train(self):
-        self.model.fit(self.train_x, self.train_y, validation_split=0.2, epochs=30)
+        self.model.fit(self.train_x, self.train_y, validation_split=0.3, epochs=15)
 
     def predict(self, grid):
         print(grid)
@@ -32,10 +42,10 @@ class hex_neural_network:
         return prediction[0]
 
     def save_model(self):
-        self.model.save("hex_model")
+        self.model.save("hex_model_" + str(self.model_size))
 
     def load_model(self):
-        self.model = keras.models.load_model("hex_model")
+        self.model = keras.models.load_model("hex_model_" + str(self.model_size))
 
     
     def preprocessing(self):
@@ -46,8 +56,8 @@ class hex_neural_network:
 
     
     def load_dataset_from_csv(self):
-        filename_x = 'hex_dataset_x.csv'
-        filename_y = 'hex_dataset_y.csv'
+        filename_x = 'hex_dataset_x_' + str(self.model_size) + '.csv'
+        filename_y = 'hex_dataset_y_' + str(self.model_size) + '.csv'
         raw_data_x = open(filename_x, 'rt')
         raw_data_y = open(filename_y, 'rt')
         self.train_x = np.loadtxt(raw_data_x, delimiter=",")
@@ -57,7 +67,8 @@ class hex_neural_network:
 
 
 if __name__ == "__main__":
-    network = hex_neural_network(16)
+    network = hex_neural_network(5)
+    network.create_model()
     network.load_dataset_from_csv()
     network.preprocessing()
     network.train()
